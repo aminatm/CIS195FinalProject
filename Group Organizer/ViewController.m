de3a29e240f10a98f0087cc7b4218bac44b53050
Aminat Musa <aminatm@wharton.upenn.edu>
2015-11-19 18:05:29 -0500

    Initial Commit

---
+++ Group Organizer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Group Organizer
+//
+//  Created by Aminat Musa on 11/19/15.
+//  Copyright © 2015 Aminat Musa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
